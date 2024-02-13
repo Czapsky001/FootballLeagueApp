e9d2be3b7d5ff786dd9ce6c52962498f6ed40611
Szymon Czap <127615744+SzymonCzap@users.noreply.github.com>
2024-02-13 17:32:02 +0100

    feat: added teamRepo

---
+++ FootballLeagueApp/FootballLeagueApp/Repositories/ITeamRepository.cs
@@ -0,0 +1,15 @@
+﻿using FootballLeagueApp.Models;
+
+namespace FootballLeagueApp.Repositories
+{
+    public interface ITeamRepository
+    {
+        Task<IEnumerable<Team>> GetTeams();
+
+        Task<Team> GetTeamByName(string name);
+
+        Task<bool> AddTeamAsync (Team team);
+
+        Task<bool> DeleteTeamAsync(Team team);
+    }
+}
---
+++ FootballLeagueApp/FootballLeagueApp/Repositories/TeamRepository.cs
@@ -0,0 +1,80 @@
+﻿using FootballLeagueApp.DatabaseConnector;
+using FootballLeagueApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballLeagueApp.Repositories
+{
+    public class TeamRepository : ITeamRepository
+    {
+        private readonly DatabaseContext ctx;
+        private readonly ILogger<TeamRepository> logger;
+
+        public TeamRepository(DatabaseContext ctx, ILogger<TeamRepository> logger)
+        {
+            this.ctx = ctx;
+            this.logger = logger;
+        }
+
+
+
+        public async Task<Team> GetTeamByName(string name)
+        {
+            try
+            {
+                return await ctx.Teams.FirstOrDefaultAsync(e => e.Name == name);
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return new Team();
+            }
+
+        }
+
+        public async Task<IEnumerable<Team>> GetTeams()
+        {
+            try
+            {
+                return await ctx.Teams.OrderBy(e => e.Points).ToListAsync();
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, ex);
+                return new List<Team>();
+            }
+        }
+
+        public async Task<bool> AddTeamAsync(Team team)
+        {
+            try
+            {
+                await ctx.Teams.AddAsync(team);
+                await ctx.SaveChangesAsync();
+                return true;
+            }catch(Exception ex)
+            {
+                logger.LogError(ex.Message, ex);
+                return false;
+            }
+
+
+        }
+
+
+        public async Task<bool> DeleteTeamAsync(Team team)
+        {
+            try
+            {
+                ctx.Teams.Remove(team);
+                await ctx.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, ex);
+                return false;
+            }
+        }
+    }
+}
