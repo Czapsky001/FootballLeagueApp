8496e2c8422a9e659fee815d5e44275f40464d30
Szymon Czap <127615744+SzymonCzap@users.noreply.github.com>
2024-02-17 22:09:49 +0100

    feat: added to favorite complete

--- ClientSide/football-league/src/Components/MainPage.jsx
+++ ClientSide/football-league/src/Components/MainPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios";
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../AuthContext/AuthContext';
 
@@ -8,15 +8,27 @@ const MainPage = () => {
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
-    axios.get("https://localhost:7233/Teams")
-      .then(response => {
-        const data = response.data.reverse();
-        setTeams(data);
-      })
+    axios.get('https://localhost:7233/Teams').then((response) => {
+      const data = response.data.reverse();
+      setTeams(data);
+    });
   }, []);
 
-  console.log(teams);
-
+  const handleButtonFavorite = (teamId) => {
+    if (!user) {
+      console.log('User not logged in');
+      return;
+    }
+    console.log(user);
+    axios
+      .post(`https://localhost:7233/api/FavoriteTeam/add?teamId=${teamId}&email=${user.email}`)
+      .then((response) => {
+        console.log('Team added to favorites:', response);
+      })
+      .catch((error) => {
+        console.error('Error adding team to favorites:', error);
+      });
+  };
   return (
     <div>
       <div className="button-container">
@@ -57,6 +69,7 @@ const MainPage = () => {
                 <td>{team.loses}</td>
                 <td>{team.draws}</td>
                 <td>{team.goals}</td>
+                <button onClick={() => handleButtonFavorite(team.id)}>AddToFavorite</button>
               </tr>
             ))}
           </tbody>
